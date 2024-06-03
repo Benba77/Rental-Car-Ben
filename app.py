31b8623617476516fa4497bed75cc32e597ecbab
Ben Aissa, Bedia (SE-A/35) <bedia.ben.aissa@volkswagen.de>
2024-06-03 13:52:16 +0200

    template added and app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-#flask
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
